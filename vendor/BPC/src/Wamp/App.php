953ef270bfb077daf69e3a37fc675519b5249566
MirageCentury <victorien.vanroye@gmail.com>
2014-10-22 10:18:01 +0200

    - ?

--- vendor/BPC/src/Wamp/App.php
+++ vendor/BPC/src/Wamp/App.php
@@ -14,10 +14,10 @@ class App extends RatchetApp {
     public function __construct(LoopInterface $loop = null, $httpHost = "projetx.nordri.fr", $port = 8080, $address = '0.0.0.0') {
         parent::__construct($httpHost, $port, $address, $loop);
         $this->main = new Main("ws.projetx");
-        $this->route("/", $this->main, array("localhost", "ip.nordri.fr", "nordri.fr", "projetx.nordri.fr", "*.nordri.fr", "*"));
+        $this->route("/", $this->main, array("localhost", "projetx.local", "ip.nordri.fr", "nordri.fr", "projetx.nordri.fr", "*.nordri.fr", "*"));
     }
 
-    public function route($path, ComponentInterface $controller, array $allowedOrigins = array("*", "nordri.fr", "ip.nordri.fr", "projetx.nordri.fr"), $httpHost = null) {
+    public function route($path, ComponentInterface $controller, array $allowedOrigins = array("*", "projetx.local", "nordri.fr", "ip.nordri.fr", "projetx.nordri.fr"), $httpHost = null) {
         return parent::route($path, $controller, $allowedOrigins, $httpHost);
     }
 
